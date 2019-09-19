f7679abdc323f4b8757b6c7c8148f778ed732d6d
Aayush Pal <47224106+pal-codes@users.noreply.github.com>
2019-09-19 10:49:13 +0530

    Added testData json file

---
+++ search.js
@@ -0,0 +1,5 @@
+const AppSearchClient = require('@elastic/app-search-node')
+const hostIdentfier = 'host-rm76oz'
+const apiKey = 'private-frkqju3d1x8uxnbciqqry3mu'
+
+const client = new AppSearchClient(hostIdentfier, apiKey)
