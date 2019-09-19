8be7aaf2f53843e0ef9a58a7135b946ce33b39f7
Aayush Pal <47224106+pal-codes@users.noreply.github.com>
2019-09-19 11:30:34 +0530

    search implemented

--- search.js
+++ search.js
@@ -3,3 +3,19 @@ const hostIdentfier = 'host-rm76oz'
 const apiKey = 'private-frkqju3d1x8uxnbciqqry3mu'
 
 const client = new AppSearchClient(hostIdentfier, apiKey)
+
+const engineName = 'ewe-search'
+const query = 'specs'
+const searchFields = { title: {}, description: {}, category: {}, amount: {}, image: {} }
+const resultFields = { title: { raw: {} }, description: { raw: {} }, category: { raw: {} }, amount: { raw: {} }, }
+const options = { search_fields: searchFields, result_fields: resultFields }
+
+client
+    .search(engineName, query, options)
+    .then(response => console.log(response))
+    .catch(error => console.log(error.errorMessages))
+
+// client
+//     .getEngine(engineName)
+//     .then(response => console.log(response))
+//     .catch(error => console.log(error.errorMessages))
