07db63d0020e9fbfb84de7e373ab828ace17db64
Aayush Pal <47224106+pal-codes@users.noreply.github.com>
2019-09-25 19:16:15 +0530

    Replaced private key with public

--- add.js
+++ add.js
@@ -1,7 +1,7 @@
 // ------ Setting up Elastic --------
 const AppSearchClient = require('@elastic/app-search-node')
 const hostIdentfier = 'host-rm76oz'
-const apiKey = 'private-frkqju3d1x8uxnbciqqry3mu'
+const apiKey = 'search-vw7g7m2ks2gisqce4qr5xb6c'
 const client = new AppSearchClient(hostIdentfier, apiKey)
 const engineName = "ewe-search"
 
