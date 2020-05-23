ffc4534e66229197df68f6374c872a4c7b76abf9
Aayush Pal <ap.aayushpal@gmail.com>
2020-05-23 22:45:20 +0530

    changes - search.js -replaced env values

--- search.js
+++ search.js
@@ -1,16 +1,17 @@
-const AppSearchClient = require('@elastic/app-search-node')
-const hostIdentfier = 'host-rm76oz'
-const apiKey = 'search-vw7g7m2ks2gisqce4qr5xb6c'
+require('dotenv').config();
+const AppSearchClient = require('@elastic/app-search-node');
+const hostIdentfier = process.env.HOST_IDENTIFIER;
+const apiKey = process.env.API_KEY;
 
-const client = new AppSearchClient(hostIdentfier, apiKey)
+const client = new AppSearchClient(hostIdentfier, apiKey);
 
-const engineName = 'ewe-search'
-const query = 'handbags'
-const searchFields = { title: {}, description: {}, category: {}, amount: {}, image: {} }
-const resultFields = { title: { raw: {} }, description: { raw: {} }, category: { raw: {} }, amount: { raw: {} }, }
-const options = { search_fields: searchFields, result_fields: resultFields }
+const engineName = 'ewe-search';
+const query = 'handbags';
+const searchFields = { title: {}, description: {}, category: {}, amount: {}, image: {} };
+const resultFields = { title: { raw: {} }, description: { raw: {} }, category: { raw: {} }, amount: { raw: {} }, };
+const options = { search_fields: searchFields, result_fields: resultFields };
 
 client
     .search(engineName, query, options)
     .then(response => console.log(response))
-    .catch(error => console.log(error.errorMessages))
+    .catch(error => console.log(error.errorMessages));
