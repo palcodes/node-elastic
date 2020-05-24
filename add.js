6e0a91db1504bcdb617809951ebfea4e591e9201
Aayush Pal <ap.aayushpal@gmail.com>
2020-05-24 09:37:23 +0530

    Update add.js

--- add.js
+++ add.js
@@ -1,13 +1,13 @@
 // ------ Setting up Elastic --------
-const AppSearchClient = require('@elastic/app-search-node')
-const hostIdentfier = 'host-rm76oz'
-const apiKey = 'search-vw7g7m2ks2gisqce4qr5xb6c'
-const client = new AppSearchClient(hostIdentfier, apiKey)
-const engineName = "ewe-search"
+const AppSearchClient = require('@elastic/app-search-node');
+const hostIdentfier = process.env.HOST_IDENTIFIER;
+const apiKey = process.env.API_KEY;
+const client = new AppSearchClient(hostIdentfier, apiKey);
+const engineName = "ewe-search";
 
 // ------ Setting up firebase ------
-import * as firebase from 'firebase'
-import * as admin from 'firebase-admin'
+import * as firebase from 'firebase';
+import * as admin from 'firebase-admin';
 const firebaseConfig = {
     apiKey: "AIzaSyAidtyO1QHi6ALPqXiCnRqp3jpYUAvYvEo",
     authDomain: "elastic-ewe.firebaseapp.com",
@@ -17,29 +17,12 @@ const firebaseConfig = {
     messagingSenderId: "842121705987",
     appId: "1:842121705987:web:7da2409c6108e8ac2b2c36"
   };
-firebase.initializeApp(firebaseConfig)
-const db = admin.firestore
+firebase.initializeApp(firebaseConfig);
+const db = admin.firestore;
 
-let dataRef = db.collection('posts').doc('aOO4vmGveqs3oYLqjWm3')
-
-// const documents = [
-//     {
-//         title: 'Floor lamp',
-//         amount: '3600',
-//         category: 'Home Decor',
-//         description: 'classic black design, 5.3 inch tall',
-//         image: 'https://instagram.fbom2-1.fna.fbcdn.net/vp/80b955f4e118a777e2e5f273cb6a46c2/5E184D54/t51.2885-15/e15/p640x640/70365617_2435798676500149_7375802252903811177_n.jpg?_nc_ht=instagram.fbom2-1.fna.fbcdn.net&_nc_cat=101 640w,https://instagram.fbom2-1.fna.fbcdn.net/vp/956eb8b7b6dc3a4ca6cff806f177b11b/5E3406AC/t51.2885-15/e15/70365617_2435798676500149_7375802252903811177_n.jpg?_nc_ht=instagram.fbom2-1.fna.fbcdn.net&_nc_cat=101 750w,https://instagram.fbom2-1.fna.fbcdn.net/vp/956eb8b7b6dc3a4ca6cff806f177b11b/5E3406AC/t51.2885-15/e15/70365617_2435798676500149_7375802252903811177_n.jpg?_nc_ht=instagram.fbom2-1.fna.fbcdn.net&_nc_cat=101 1080w',
-//     },
-//     {
-//         title: 'Green Dress',
-//         amount: '5990',
-//         category: 'clothing',
-//         description: '1 month old, wore by a model for once, premium collection ZARA',
-//         image: 'https://instagram.fbom2-1.fna.fbcdn.net/vp/826694523df4318f604b2231fd4b84f1/5E05BF5C/t51.2885-15/sh0.08/e35/p640x640/70357512_165199457993643_6283847501323784947_n.jpg?_nc_ht=instagram.fbom2-1.fna.fbcdn.net&_nc_cat=103 640w,https://instagram.fbom2-1.fna.fbcdn.net/vp/065121124655437fbb5485fbd806e3a3/5E21DD5C/t51.2885-15/sh0.08/e35/p750x750/70357512_165199457993643_6283847501323784947_n.jpg?_nc_ht=instagram.fbom2-1.fna.fbcdn.net&_nc_cat=103 750w,https://instagram.fbom2-1.fna.fbcdn.net/vp/758be5b90f8247248b040bf0d6098489/5E047FAA/t51.2885-15/e35/70357512_165199457993643_6283847501323784947_n.jpg?_nc_ht=instagram.fbom2-1.fna.fbcdn.net&_nc_cat=103 1080w',
-//     },
-// ]
+let dataRef = db.collection('posts').doc('aOO4vmGveqs3oYLqjWm3');
 
 client
     .indexDocuments(engineName, documents)
     .then(response => console.log(response))
-    .catch(error => console.log(error))
+    .catch(error => console.log(error));
